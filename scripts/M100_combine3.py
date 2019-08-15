49f62fe28dc8175a4a728df11e57a96f4e5a4dfa
Peter Teuben <teuben@gmail.com>
2019-08-15 17:30:44 +0200

    remove some W related keywords

--- scripts/M100_combine3.py
+++ scripts/M100_combine3.py
@@ -1,4 +1,4 @@
-# SDINT example,
+# SDINT example:
 
 # First we combine the 7m INT with the 12m TP data; we use the benchmark M100 data
 
@@ -16,10 +16,11 @@ phasecenter = 'J2000 12h22m54.900s +15d49m15.000s'
 imsize      = 800
 cell        = '0.5arcsec'
 reffreq     = '115GHz'
-dishdia     = 12.0 # in meters
+dishdia     = 10.0 # in meters
 niter       = 10000
 cycleniter  = 500
-scales      = [0,12,20,40,60,80,100]
+#scales      = [0,12,20,40,60,80,100]
+scales      = [0,5,20]
 pblimit     = 0.2
 mythresh    = '.050mJy'
 mask        = 'M100.im.masklist'  ## region file, or mask image with 1,0.
@@ -31,37 +32,38 @@ os.system('rm -rf '+jointname+'*')
 
 jointim = SDINT_imager(vis=vis,
                            
-                           sdimage=sdimage,
-                           sdpsf=sdpsf,
-                           sdgain=1.0,
-                           dishdia=dishdia,
-
-                           usedata='sdint',  ## or 'sdonly' or 'intonly'
+                       sdimage=sdimage,
+                       sdpsf=sdpsf,
+                       sdgain=1.0,
+                       dishdia=dishdia,
+                       
+                       usedata='sdint',  ## or 'sdonly' or 'intonly'
                            
-                           imagename=jointname,
-                           imsize=imsize,
-                           cell=cell,
-                           phasecenter=phasecenter,
-                           weighting='natural',
-                           specmode=specmode,
+                       imagename=jointname,
+                       imsize=imsize,
+                       cell=cell,
+                       phasecenter=phasecenter,
+                       weighting='natural',
+                       specmode=specmode,
+                       
+                       gridder=gridder,
+                       nchan=70,
+                       reffreq=reffreq,
+                       width='',
+                       pblimit=pblimit, 
+                       # interpolation='nearest',
+                       # wprojplanes=1,
                            
-                           gridder=gridder,
-                           nchan=70,
-                           reffreq=reffreq,
-                           width='',
-                           pblimit=pblimit, 
-                           interpolation='nearest',
-                           wprojplanes=1,
+                       deconvolver=deconvolver,
+                       scales=scales,
+                       # nterms=2,
+                       pbmask=0.2,
                            
-                           deconvolver=deconvolver,
-                           scales=scales,
-                           nterms=2,
-                           pbmask=0.2,
-                           
-                           niter=niter,
-                           cycleniter=cycleniter,
-                           threshold=mythresh,
-                           mask=mask)
+                       niter=niter,
+                       cycleniter=cycleniter,
+                       threshold=mythresh,
+                       # mask=mask,
+)
     
 
 decname = jointim.do_reconstruct()
