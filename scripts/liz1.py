bd96034ee476e37f4bca03fa01a57321e867a638
Peter Teuben <teuben@gmail.com>
2019-08-19 14:01:30 +0200

    npointing

--- scripts/liz1.py
+++ scripts/liz1.py
@@ -1,23 +1,25 @@
 # N346 debugging
+#
 
-p = qac_ms_ptg('region3by3-7M-leiden-avg.ms','12m.ptg')
+npoint = 500
+p      = qac_ms_ptg('region3by3-7M-leiden-avg.ms','12m.ptg')
 
 if False:
     # this will use all 1345 pointings !!!
     tp2vis('new.cube.spw23-forLeiden.image','new.cube.spw23-forLeiden.image.ms','12m.ptg',rms=0.5)
 else:
     # use fewer to test if the crash is due to too many fields/pointings
-    p1 = p[:20]
+    p1 = p[:npoint]
     tp2vis('new.cube.spw23-forLeiden.image','new.cube.spw23-forLeiden.image.ms',p1,rms=0.5)    
 
 concat(vis=['region3by3-7M-leiden-avg.ms','new.cube.spw23-forLeiden.image.ms'], concatvis='all.ms',copypointing=False)
 
-field = '0~4499'      # fails
-field = '0~20'        # ok
-field = '0~1345'      # this still includes calibrators.
+field = '0~4499'          # fails
+field = '0~1345'          # this still includes calibrators.
+field = '0~%d' % npoint   # ok
 
 vis = 'all.ms'
-vis = 'region3by3-7M-leiden-avg.ms'
+# vis = 'region3by3-7M-leiden-avg.ms'
 
 tclean(vis = vis,
        imagename = 'all_tp2vis.image',
