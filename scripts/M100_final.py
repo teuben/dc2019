0e5007e5969d2e50a2e6b4b4215dd84ad8932fb4
Peter Teuben <teuben@gmail.com>
2019-09-02 21:16:59 -0400

    add some boxes where needed to do a better flux comparison

--- scripts/M100_final.py
+++ scripts/M100_final.py
@@ -6,29 +6,36 @@ tpim  = '../M100_TP_CO_cube.bl.image'
 ms07  = '../M100_aver_7.ms'
 ms12  = '../M100_aver_12.ms'
 
+box1  = '219,148,612,579'     # casaguide box in their 800x800 image
+box   = box1
+
 os.chdir(pdir)
 
 if True:
     # plot comparing flux of TP ; use [::-1] when an axis needs to be reverted
     f0a =  imstat(tpim,                                    axes=[0,1])['flux']
+    f0b =  imstat('../M100/M100_TP_CO_cube.regrid',        axes=[0,1])['flux'][::-1]
+    f0c =  imstat('../M100/M100_TP_CO_cube.regrid',        axes=[0,1], box=box)['flux'][::-1]     # TP
+    f0e =  imstat('../M100/M100_TP_CO_cube.regrid.subim',  axes=[0,1])['flux'][::-1]
+    
     f1a =  imstat('test6/tp0/clean0/dirtymap.image',       axes=[0,1])['flux']
     f1b =  imstat('test6/tp1/clean0/dirtymap.image',       axes=[0,1])['flux']
     f1c =  imstat('test6/M100_TP_CO_cube.bl.smo',          axes=[0,1])['flux']
     f1d =  imstat('test6/M100_TP_CO_cube.bl.smo',          axes=[0,1], box=box)['flux']    
-    f2a =  imstat('test6/tp0/clean0/dirtymap.image.pbcor', axes=[0,1])['flux']
-    f2b =  imstat('test6/tp1/clean0/dirtymap.image.pbcor', axes=[0,1])['flux']
-    f2c =  imstat('test6/tp2/clean0/dirtymap.image.pbcor', axes=[0,1])['flux']
+    f2a =  imstat('test6/tp0/clean0/dirtymap.image.pbcor', axes=[0,1], box=box)['flux']
+    f2b =  imstat('test6/tp1/clean0/dirtymap.image.pbcor', axes=[0,1], box=box)['flux']
+    f2c =  imstat('test6/tp2/clean0/dirtymap.image.pbcor', axes=[0,1], box=box)['flux']
     f3a =  imstat('test6/tp.model',                        axes=[0,1])['sum']
 
-    f4a =  imstat('M100sdint_7.joint.cube.image/',         axes=[0,1])['flux']
-    f4b =  imstat('M100sdint_7.sd.cube.image/',            axes=[0,1])['flux']
+    f4a =  imstat('M100sdint_7.joint.cube.image',          axes=[0,1], box=box)['flux']
+    f4b =  imstat('M100sdint_7.sd.cube.image',             axes=[0,1], box=box)['flux']
 
-    f5a =  imstat('M100sdint_12.joint.cube.image/',        axes=[0,1])['flux']
-    f5b =  imstat('M100sdint_12.sd.cube.image/',           axes=[0,1])['flux']
+    f5a =  imstat('M100sdint_12.joint.cube.image',         axes=[0,1], box=box)['flux']
+    f5b =  imstat('M100sdint_12.sd.cube.image',            axes=[0,1], box=box)['flux']
 
-    f6a =  imstat('M100_Feather_CO.image.pbcor',           axes=[0,1])['flux'][::-1]
+    f6a =  imstat('M100_Feather_CO.image.pbcor',           axes=[0,1])['flux'][::-1]              # Feather
     f6b =  imstat('../M100/M100_Feather_CO.image.pbcor',   axes=[0,1])['flux'][::-1]
-
+    
     f7a =  imstat('test6/clean3/tpint.image.pbcor',        axes=[0,1])['flux']
     f7b =  imstat('test6/clean3/tpint_3.image.pbcor',      axes=[0,1])['flux']
     f7c =  imstat('test6/clean4/tpint.image.pbcor',        axes=[0,1])['flux']    # crazy high
@@ -36,20 +43,29 @@ if True:
     f7e =  imstat('test6/clean6/tpint_2.tweak.image',      axes=[0,1])['flux']    #
 
     label = []
-    label.append("TP")                    # f0a
-    label.append("TP vis deconv=True")     # f2a
-    label.append("TP deconvolver")       # f2b
-    label.append("TP model")              # f3a
-    plot2a([f0a,f2a,f2b,f3a],'Flux Comparison M100','flux-cmp1.png', dv=5, label=label)
+    label.append("TP")          # f0a
+    label.append("TP-regrid")   # f0b
+    label.append("TP-box")      # f0c
+    label.append("diff")
+    f0d = f0b-f0c
+    plot2a([f0a,f0b,f0c,f0d],'Flux Comparison M100 for TP','flux-cmp0.png', dv=5, label=label)    
 
     label = []
-    label.append("TP")                    # f0a
+    label.append("TP")                    # f0c
+    label.append("TP vis deconv=True")    # f2a
+    label.append("TP deconvolver")        # f2b
+    label.append("TP model")              # f3a
+    plot2a([f0c,f2a,f2b,f3a],'Flux Comparison M100','flux-cmp1.png', dv=5, label=label)
+
+    label = []
+    label.append("TP")                    # f0c
     label.append("SDINT 7m")
-    label.append("SDINT 7m SD")
     label.append("SDINT 7+12m")
-    label.append("SDINT 7+12m SD")
+    label.append("7 vs 7+12 diff")
+    f4d = f4a-f5a
+    f5d = f0c-f4b      # checked: these are identical
     
-    plot2a([f0a,f4a,f4b,f5a,f5b],'Flux Comparison M100','flux-cmp2.png', dv=5, label=label)
+    plot2a([f0c,f4a,f5a,f4d],'Flux Comparison M100 SDINT','flux-cmp2.png', dv=5, label=label)
 
     label = []
     label.append("TP")                    # f0a
@@ -64,14 +80,14 @@ if True:
     label.append("Feather")               # f6a
     label.append("Feather-full")          # f6b
 
-    plot2a([f0a,f6a,f6b],'Flux Comparison M100','flux-cmp4.png', dv=5, label=label)
+    plot2a([f0a,f6a,f6b],'Flux Comparison M100 sanity check','flux-cmp4.png', dv=5, label=label)
 
     label = []
     label.append("TP")                    # f0a
     label.append("Feather")               # f6a
-    label.append("SDINT 7+12m")
+    label.append("SDINT 7+12m")           # f5a
     
-    plot2a([f0a,f6a,f5a],'Flux Comparison M100','flux-cmp5.png', dv=5, label=label)
+    plot2a([f0c,f6a,f5a],'Flux Comparison M100','flux-cmp5.png', dv=5, label=label)
 
 
 if True:
