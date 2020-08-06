e78b93b992ee0e702109763713b748f22ac449ec
toshi-takagi <68998283+toshi-takagi@users.noreply.github.com>
2020-08-06 20:54:56 +0900

    Add files via upload

--- scripts/simScript/ACASimulation.py
+++ scripts/simScript/ACASimulation.py
@@ -38,6 +38,7 @@ if not isThere and fitsFile == 'M51ha.fits':
 hd = imhead(imagename= fitsFile)
 pixSize = abs(hd['incr'][0]*180/3.141592*3600) # arcsec 
 
+"""
 # RMS estimate with simple sigma clipping
 imsize = hd['shape']
 box = '0,0,%s,%s'%(str(imsize[0]-1),str(imsize[1]-1))
@@ -54,13 +55,14 @@ for i in range(niter):
     rms = np.std( data[nonzero*noise])
 
 snr = np.max(imgData)/rms 
+"""
 
 print " "
 print "### Original FITS specification : ", fitsFile 
 print "# pixel scale [arcsec] = ", pixSize
 print "# image shape = ", hd['shape']
 print "# image size [arcsec] = ", pixSize*hd['shape'][0]
-print "# SNR of image max = ", snr 
+#print "# SNR of image max = ", snr 
 print "###########################################"
 
 input_line = raw_input("Check the image spec [RETURN/n] :")
@@ -69,45 +71,55 @@ if input_line:
 
 
 
-# [12m 7m] beam size 
+# [12m 7m] primary beam size 
 diam = np.array([12., 7.])
 wave = 2.99792e8/ (float(incenter.split("GHz")[0])*1.e9)
 hpbw = 1.02 * wave / diam *180/3.141592 *3600 # arcsec
 
+
 # number of pointing with 0.5PB spacing
 npoint12 = int((float(mapsize.split("arcsec")[0]) / hpbw[0] *2))**2
 npoint7 = int((float(mapsize.split("arcsec")[0]) / hpbw[1] *2))**2
+"""
 if not totaltime12:
     totaltime12 = str(npoint12 * float(integration.split("s")[0]))+'s'
 if not totaltime7:
     totaltime7 = str(npoint7 * float(integration.split("s")[0])*timeRatio[1])+'s'
 if not totaltimeTP: 
     totaltimeTP = str(npoint12 * float(integration.split("s")[0])*timeRatio[2])+'s'
+"""
 
 ## Sensitivity setting 
 #
 
 beam = []
-mrs = []
+mrs = [] # Maximum Recoverable Scale 
 for i in range(len(config)):
     beam.append(aU.estimateSynthesizedBeamForConfig(config[i], incenter))
     mrs.append(aU.estimateMRSForConfig(config[i], incenter))
 
-autoSens = False
-if not inbright: 
-    print " "
-    print "# Sensitivity calculation .... "
-    print " "
-    rmsVis = aU.sensitivity(incenter, inwidth, integration, pwv=pwv, antennalist = config[0])
-    maxval = rmsVis*snr # Maximum pixel value in skymodel 
-    inbright = str(maxval)+" Jy/pixel" # This is rough estimate (i.e. beamsize is not considered) 
-    autoSens=True
+
+# Number of pixel per beam (estimated)
+pixScale  = (float(incell.split("arcsec")[0])) # arcsec 
+beamSize = beam[0]
+npix_per_beam = (beamSize/pixScale)**2
+
+
+
+print " "
+print "# Sensitivity calculation .... "
+print " "
+intTime = str( float(integration.split("s")[0])*float(totaltime12))+'s'
+rms12m = aU.sensitivity(incenter, inwidth, intTime, pwv=pwv, antennalist = config[0])
+
+
 
 print " "
 print "### Sky Model specification : "
 print "# Project name = ", project
 print "# pixel scale [arcsec] = ", incell 
-print "# Peak intensity = ", inbright 
+print "# Peak intensity [Jy/pix] = ", inbright
+print "# Estimated peak [Jy/beam] = ", float(inbright)*npix_per_beam
 print "# image shape = ", hd['shape']
 print "# image size [arcsec] = ", hd['shape'][0]*float(incell.split("arcsec")[0])
 print " "
@@ -122,8 +134,7 @@ print "# Integration time = ", integration
 print "# 12m HPBW [arcsec] = ", hpbw[0]
 print "# 12m Number of pointing = ", npoint12 # simple guess 
 print "# 12m Total time = ", totaltime12
-if autoSens:
-  print "# 12m Array sensitivity [Jy] = ", rmsVis
+print "# 12m Array sensitivity [mJy] = ", rms12m*1000
 print "# 7m HPBW [arcsec] = ", hpbw[1]
 print "# 7m Number of pointing = ", npoint7 # simple guess 
 print "# 7m Total time = ", totaltime7
