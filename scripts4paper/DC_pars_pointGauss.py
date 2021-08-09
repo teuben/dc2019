c8c012a81d236c7b8a6bd1520608a3df7a405e9c
LydiaMF <lamiacrudelis@gmx.de>
2021-08-09 15:38:18 +0200

    parameters skymodel and momchans added

--- scripts4paper/DC_pars_pointGauss.py
+++ scripts4paper/DC_pars_pointGauss.py
@@ -16,7 +16,6 @@ step_title = {0: 'Concat',
               8: 'Assessment of the combination results'
               }
 
-
 thesteps=[0,1,2,3,4,5,6,7,8]
 #thesteps=[7]
 
@@ -60,6 +59,8 @@ thevis.extend(a7m)
 
 weightscale = weight12m
 weightscale.extend(weight7m)
+
+skymodel=a12m[0].replace('.ms','.skymodel')    # model used for simulating the observation, expected to be CASA-imported
 
 ##### non interactive - end #####
 
@@ -115,6 +116,10 @@ cont_chans = ''    # line free channels for cube rms estimation
 sdmasklev  = 0.3   # maximum x this factor = threshold for SD mask
 
 
+momchans = ''      # channels to compute moment maps (integrated intensity, etc.) 
+
+                     
+########## general tclean parameters
 
 general_tclean_param = dict(#overwrite  = overwrite,
                            spw         = '0', 
