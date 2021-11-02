559cdce313dd6b9530bc2f8f23c1bef9978e4608
Peter Teuben <teuben@gmail.com>
2021-11-02 08:09:51 -0400

    warning

--- scripts/M100_getdata.sh
+++ scripts/M100_getdata.sh
@@ -1,4 +1,5 @@
 #  see also https://casaguides.nrao.edu/index.php?title=M100_Band3
+#
 
 wget https://bulk.cv.nrao.edu/almadata/sciver/M100Band3_12m/M100_Band3_12m_CalibratedData.tgz
 wget https://bulk.cv.nrao.edu/almadata/sciver/M100Band3ACA/M100_Band3_7m_CalibratedData.tgz
@@ -7,6 +8,8 @@ wget https://bulk.cv.nrao.edu/almadata/sciver/M100Band3ACA/M100_Band3_ACA_Refere
 tar xvfz M100_Band3_12m_CalibratedData.tgz
 tar xvfz M100_Band3_7m_CalibratedData.tgz
 tar xvfz M100_Band3_ACA_ReferenceImages_5.1.tgz
+
+#  Warning:  re-executing this script should remove the targets here
 
 mv M100_Band3_12m_CalibratedData/M100_Band3_12m_CalibratedData.ms   .
 mv M100_Band3_7m_CalibratedData/M100_Band3_7m_CalibratedData.ms     .
