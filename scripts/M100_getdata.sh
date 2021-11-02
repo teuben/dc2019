5d4b4d6be2924414ba56c1143c5bb1191cece206
Peter Teuben <teuben@gmail.com>
2021-11-02 08:17:39 -0400

    add -c  continue option

--- scripts/M100_getdata.sh
+++ scripts/M100_getdata.sh
@@ -1,9 +1,9 @@
 #  see also https://casaguides.nrao.edu/index.php?title=M100_Band3
 #
 
-wget https://bulk.cv.nrao.edu/almadata/sciver/M100Band3_12m/M100_Band3_12m_CalibratedData.tgz
-wget https://bulk.cv.nrao.edu/almadata/sciver/M100Band3ACA/M100_Band3_7m_CalibratedData.tgz
-wget https://bulk.cv.nrao.edu/almadata/sciver/M100Band3ACA/M100_Band3_ACA_ReferenceImages_5.1.tgz
+wget -c https://bulk.cv.nrao.edu/almadata/sciver/M100Band3_12m/M100_Band3_12m_CalibratedData.tgz
+wget -c https://bulk.cv.nrao.edu/almadata/sciver/M100Band3ACA/M100_Band3_7m_CalibratedData.tgz
+wget -c https://bulk.cv.nrao.edu/almadata/sciver/M100Band3ACA/M100_Band3_ACA_ReferenceImages_5.1.tgz
 
 tar xvfz M100_Band3_12m_CalibratedData.tgz
 tar xvfz M100_Band3_7m_CalibratedData.tgz
