36cfa484c272b0a4da51d69a5fabba946a7b650a
LydiaMF <lamiacrudelis@gmx.de>
2022-03-02 13:32:06 +0100

    DC_pars_pointGauss - added new fniteronusermask and threshregion parameter and rearrange order corresponding to DC_pars_Template.py

--- scripts4paper/DC_pars_pointGauss.py
+++ scripts4paper/DC_pars_pointGauss.py
@@ -16,12 +16,12 @@ step_title = {0: 'Concat',
               8: 'Assessment of the combination results'
               }
 
-#thesteps=[0,1,2,3,4,5,6,7,8]
-thesteps=[1,5]
+thesteps=[1,2,3,4,5,6,7,8]
+#thesteps=[7]
 
-######## collect only the product name (i.e. run assessment on already existing combination products)?          
 dryrun = False    # False to execute combination, True to gather filenames only
  
+## Paths to the input and output files
 
 #  you can use _s4p_data if you want to use the configure'd setup,
 #  but feel free to override
@@ -32,33 +32,21 @@ pathtoconcat = _s4p_data + '/pointSrc.sim/pointSrcGauss_3L/'
 pathtoimage  = _s4p_work + '/pointGauss/'
 
 
-
-# setup for concat (the optional step 0)
+# setup for concat (step 0)
 
 a12m=[pathtoconcat + 'pointSrcGauss_3L.alma.cycle6.1.2018-10-02.ms',
       pathtoconcat + 'pointSrcGauss_3L.alma.cycle6.4.2018-10-02.ms'
       ]
-   
-weight12m = [1., 1.]
-  
-      
 a7m =[pathtoconcat + 'pointSrcGauss_3L.aca.cycle6.2018-10-05.ms'
-      ]
-
+      ]         
+weight12m = [1., 1.]
 weight7m = [0.116]  # weigthing for SIMULATED data !
 
-
-
-skymodel=a12m[0].replace('.ms','.skymodel')    # model used for simulating the observation, expected to be CASA-imported
-
-
-
-#  the concatenated MS 
 concatms     = pathtoimage + 'pointGauss.alma.all_int-weighted.ms'       # path and name of concatenated file
 
 
 
-############# input to combination methods ###########
+## Files and base-names used by the combination methods (steps 1 - 8)
 
 vis            = ''                                    # set to '' if concatms is to be used, else define your own ms-file
 sdimage_input  = pathtoconcat + 'pointSrcGauss_3L.sd.image'
@@ -66,93 +54,107 @@ imbase         = pathtoimage + 'pointGauss'            # path + image base name
 sdbase         = pathtoimage + 'pointGauss'            # path + sd image base name
 
 
-# TP2VIS related:
+
+## Setup of the clean parameters (steps 1, 2, 5, 6, 7)
+
+### general  - data selection and image parameters
+
+t_spw         = '0' 
+t_field       = '0~68' 
+t_imsize      = [1120] 
+t_cell        = '0.21arcsec'  # add 'arcsec' to value
+t_phasecenter = 'J2000 12:00:00 -35.00.00.0000'  # pointing / mosaic center
+
+
+### spectral mode - mfs -cube
+
+mode       = 'mfs'            # 'mfs' or 'cube'
+specsetup  = 'INTpar'         # 'SDpar' (use SD cube's spectral setup) or 'INTpar' (user defined cube setup)
+					          
+t_start    = 0                # e.g.,  0 (first chan),  '10km/s',  '10MHz'
+t_width    = 1                # e.g.,  1 (one chan),  '-100km/s', '200GHz'
+t_nchan    = -1               # e.g., -1 (all chans),        20 ,     100
+t_restfreq = ''               # e.g., '234.567GHz'
+                 			          
+startchan  = 30               # None, or e.g., 30, start-value of the SD image channel range you want to cut out 
+endchan    = 39               # None, or e.g., 39,   end-value of the SD image channel range you want to cut out
+
+
+### multiscale                
+	
+mscale     = 'MS'             # 'MS' (multiscale) or 'HB' (hogbom; MTMFS in SDINT by default!)) 
+t_maxscale = -1               # for 'MS': number for largest scale size ('arcsec') expected in source
+
+
+### user interaction and iterations and threshold
+
+inter       = 'nIA'           # interactive ('IA') or non-interactive ('nIA')
+nit         = 10000000        # number of iterations
+t_threshold = '0.001Jy'       # = 7*RMS in box from step 1 - but make mask at another level i.e. RMSfactor 1500 
+
+
+### masking
+
+masking             = 'SD-INT-AM'    # 'UM' (user mask), 'SD-INT-AM' (SD+AM mask)), 'AM' ('auto-multithresh') or 'PB' (primary beam)
+t_mask              = ''      # specify for 'UM', mask name
+t_pbmask            = 0.4     # specify for 'PM', cut-off level
+t_sidelobethreshold = 2.0     # specify for 'AM', default: 2.0 
+t_noisethreshold    = 4.25    # specify for 'AM', default: 4.25 
+t_lownoisethreshold = 1.5     # specify for 'AM', default: 1.5             
+t_minbeamfrac       = 0.3     # specify for 'AM', default: 0.3 
+t_growiterations    = 75      # specify for 'AM', default: 75 
+t_negativethreshold = 0.0     # specify for 'AM', default: 0.0 
+
+
+#### SD-INT-AM mask fine-tuning (step 1)
+
+smoothing    = 5                 # smoothing of the threshold mask (by 'smoothing x beam')
+threshregion = '924,48,1111,169' # emission free region in template continuum or channel image
+RMSfactor    = 1500.0            # continuum rms level (not noise from emission-free regions but entire image)
+cube_rms     = 3                 # cube noise (true noise) x this factor
+cont_chans   = ''                # line free channels for cube rms estimation
+sdmasklev    = 0.3               # maximum x this factor = threshold for SD mask
+	
+
+#### SD-INT-AM masks for all methods using tclean etc (steps 2, 5 - 7)
+# options: 'SD', 'INT', 'combined'
+
+tclean_SDAMmask = 'INT'  
+hybrid_SDAMmask = 'INT'     
+sdint_SDAMmask  = 'INT'     
+TP2VIS_SDAMmask = 'INT'     
+fniteronusermask = 0.3 
+
+
+### SDINT options (step 6)
+
+sdpsf   = ''
+dishdia = 12.0
+         
+                 
+## SD factors for all methods (steps (3 - 7)
+               
+sdfac   = [1.0]               # feather parameter
+SSCfac  = [1.0]               # Faridani parameter
+sdfac_h = [1.0]               # Hybrid feather paramteter
+sdg     = [1.0]               # SDINT parameter
+TPfac   = [1.0]               # TP2VIS parameter
+
+          
+## TP2VIS related setup (step 7)
+
 TPpointingTemplate        = a12m[0]
 listobsOutput             = imbase+'.12m.log'
 TPpointinglist            = imbase+'.12m.ptg'
 TPpointinglistAlternative = 'user-defined.ptg' 
 
-TPnoiseRegion             = '150,200,150,200'  # in unregridded SD image (i.e. sdreordered = sdbase +'.SD_ro.image')
-TPnoiseChannels           = '2~5'              # in unregridded and un-cut SD cube (i.e. sdreordered = sdbase +'.SD_ro.image')!
+TPnoiseRegion             = '41,41,100,100'  # in unregridded SD image (i.e. sdreordered = sdbase +'.SD_ro.image')
+TPnoiseChannels           = '2~5'            # in unregridded and un-cut SD cube (i.e. sdreordered = sdbase +'.SD_ro.image')!
 
-
-
-mode      = 'mfs'      # 'mfs' or 'cube'
-mscale    = 'MS'       # 'MS' (multiscale) or 'HB' (hogbom; MTMFS in SDINT by default!)) 
-masking   = 'SD-AM'    # 'UM' (user mask), 'SD-AM' (SD+AM mask)), 'AM' ('auto-multithresh') or 'PB' (primary beam)
-inter     = 'nIA'      # interactive ('IA') or non-interactive ('nIA')
-nit       = 10#1000000          # max = 9.9 * 10**9 
-
-specsetup =  'INTpar'  # 'SDpar' (use SD cube's spectral setup) or 'INTpar' (user defined cube setup)
-
-# if "SDpar", want to use just a channel-cut-out of the SD image? , 
-# else set to None (None automatically for 'INTpar'
-
-startchan = 30  #None  # start-value of the SD image channel range you want to cut out 
-endchan   = 39  #None  #   end-value of the SD image channel range you want to cut out
-
-# resulting name part looks like
-# cleansetup = '.'+ mode +'_'+ specsetup +'_'+ mscale +'_'+ masking +'_'+ inter +'_n'+ str(nit)
-
-
-smoothing  = 5     # smoothing of the threshold mask (by 'smoothing x beam')
-RMSfactor  = 0.5   # continuum rms level (not noise from emission-free regions but entire image)
-cube_rms   = 3     # cube noise (true noise) x this factor
-cont_chans = ''    # line free channels for cube rms estimation
-sdmasklev  = 0.3   # maximum x this factor = threshold for SD mask
-
-
-momchans = ''      # channels to compute moment maps (integrated intensity, etc.) 
-
-                     
-########## general tclean parameters
-
-
-t_spw         = '0' 
-t_field       = '0~68' 
-t_imsize      = [1120] 
-t_cell        = '0.21arcsec'    # arcsec
-t_phasecenter = 'J2000 12:00:00 -35.00.00.0000'            
-t_start       = 0 
-t_width       = 1 
-t_nchan       = -1 
-t_restfreq    = ''
-t_threshold   = ''               # SDINT: None 
-t_maxscale    = -1 #10.              # recommendations/explanations 
-t_mask        = '' 
-t_pbmask      = 0.4
-t_sidelobethreshold = 2.0 
-t_noisethreshold    = 4.25 
-t_lownoisethreshold = 1.5               
-t_minbeamfrac       = 0.3 
-t_growiterations    = 75 
-t_negativethreshold = 0.0 
- 
-
-########## sdint parameters 
-
-sdpsf   = ''
-dishdia = 12.0
-                 
-
-########### SD-AM masks for all methods using tclean etc.:                       
-
-# options: 'SD', 'INT', 'combined'
-
-tclean_SDAMmask = 'INT'  
-hybrid_SDAMmask = 'SD'     
-sdint_SDAMmask  = 'INT'     
-TP2VIS_SDAMmask = 'INT'     
-
-
-########### SD factors for all methods:                       
-               
-sdfac   = [1.0]          # feather parameter
-SSCfac  = [1.0]          # Faridani parameter
-sdfac_h = [1.0]          # Hybrid feather paramteter
-sdg     = [1.0]          # SDINT parameter
-TPfac   = [1.0]          # TP2VIS parameter
+  
+## Assessment related (step 8)
           
+momchans = ''                 # channels to compute moment maps (integrated intensity, etc.) 
+mapchan = None                # cube channel (integer) of interest to use for assessment in step 8
 
-          
-
+skymodel=a12m[0].replace('.ms','.skymodel')    # model used for simulating the observation, expected to be CASA-imported
